3b74ae0115e20c2f32a174db3448208ceba6978b
embryobirthday <alexinverda@gmail.com>
2024-06-27 14:14:15 -0400

    pokemon card draft

--- PokemonIRL/main.js
+++ PokemonIRL/main.js
@@ -1,12 +1,14 @@
-//fetch function for data
+//fetch function for pokemon data
 const fetchPokemonData = async (url, options = {}) => {
     try {
         const response = await fetch(url, options);
 
         if (!response.ok) {
+            console.log(`Response: FAIL!`)
             throw new Error(`Fetch failed. ${response.status} ${response.statusText}`)
+        } else if (response.ok === true) {
+            console.log('Response: OK!')
         }
-
         const isJson = (response.headers.get('content-type') || '').includes('application/json')
         let data = isJson ? await response.json() : await response.text()
         console.log(data)
@@ -18,6 +20,7 @@ const fetchPokemonData = async (url, options = {}) => {
         return [null, error];
     }
 }
+
 
 
 // JavaScript to handle button clicks and form submissions can go here
@@ -34,13 +37,15 @@ document.getElementById('about-devs-button').addEventListener('click', () => {
 });
 
 // Mock dataset of Pokémon by continent
-const pokemonData = {
+const pokemonInRegionData = {
     "Africa": ["Sandile", "Torterra", "Hippopotas"],
     "Asia": ["Pikachu", "Charmander", "Bulbasaur"],
     "Europe": ["Rattata", "Eevee", "Snorlax"],
     "North America": ["Squirtle", "Pidgey", "Jigglypuff"],
+    "South America": ["Ludicolo", "Tropius", "Yanma"],
+    "Australia": ['Victreebel', "Sharpedo", "Houndoom"],
     "Oceania": ["Lapras", "Gyarados", "Kangaskhan"],
-    "South America": ["Ludicolo", "Tropius", "Yanma"]
+    "Antarctica": ["Regice", "Cubchoo", "Snom"]
 };
 
 
@@ -49,28 +54,48 @@ document.getElementById('enter-button').addEventListener('click', () => {
     const continent = document.getElementById('continent').value;
     const resultsDiv = document.getElementById('results');
 
-    if (pokemonData[continent]) {
-        resultsDiv.innerHTML = `<h2 aria-label="search-result-header" id="searchResultHeader">Pokémon in ${continent}:</h2><ul>` + pokemonData[continent].map(pokemon => `<li class='listed-pokemon'>${pokemon}</li>`).join('') + `</ul>`;
+    if (pokemonInRegionData[continent]) {
+        resultsDiv.innerHTML = `<h2 aria-label="search-result-header" id="searchResultHeader">Pokémon in ${continent}:</h2><ul>` + pokemonInRegionData[continent].map(pokemon =>
+            `<section id="pokemon-container"> 
+                                                                                                                                        <img id="pokemon-sprite" src=""> 
+                                                                                                                                        <li class='listed-pokemon'> ${pokemon} 
+                                                                                                                                       <section id="types"> <button id="type1" class="type-button">Type 1</button>  <button id="type2" class="type-button">Type 2</button> </section>
+                                                                                                                                        <button id="pokemon-more-info">Learn more</button> 
+                                                                                                                                        </li>
+                                                                                                                                        </section>`).join('') + `</ul>`;
 
 
 
 
         // Add click event listeners to each listed Pokémon
         const listedPokemons = resultsDiv.querySelectorAll('.listed-pokemon');
-        
+
         console.log(listedPokemons)
         listedPokemons.forEach(pokemonElement => {
             pokemonElement.addEventListener('click', () => {
-                console.log("what")
                 const pokemon = pokemonElement.textContent.toLowerCase()
-                const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+                //calling that fetch using a url
+                const pokemonGenInfoUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+                const genPokemonData = fetchPokemonData(pokemonGenInfoUrl);
+                console.log(genPokemonData)
 
-                fetchPokemonData(url);
+                //writing promises to fetch
+
+                const pokemonSprite = fetchPokemonData(pokemonSpriteUrl).then(
+                    data => {
+                        return data.sprites
+                    }
+                )
+                console.log(pokemonSprite)
+
+
+
+
 
             });
         });
     } else {
-        resultsDiv.innerHTML = `<p>No Pokémon data available for ${continent}.</p>`;
+        resultsDiv.innerHTML = `<p> No Pokémon data available for ${continent}. </p> `;
     }
 });
 
@@ -82,60 +107,60 @@ document.getElementById('enter-button').addEventListener('click', () => {
 
 
 
-// <!-- Testing the fetch -->
+// // <!-- Testing the fetch -->
 
 
-async function testRoute(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log('data:', data);
-}
+// async function testRoute(url) {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log('data:', data);
+// }
 
 
-const url1 = `https://pokeapi.co/api/v2/pokemon/ditto`;
+// const url1 = `https://pokeapi.co/api/v2/pokemon/ditto`;
 
-testRoute(url1);
+// testRoute(url1);
 
-//creating a fetch function for pokemon types and testing it
-const typeTest = () => {
+// //creating a fetch function for pokemon types and testing it
+// const typeTest = () => {
 
-    //defining a url variable which holds the url to a pokemon ground type database
-    const urlGround = (`https://pokeapi.co/api/v2/type/5`);
+//     //defining a url variable which holds the url to a pokemon ground type database
+//     const urlGround = (`https://pokeapi.co/api/v2/type/5`);
 
-    //returning a fetch promise
-    return fetch(urlGround)
-        //chaining .then promises 
-        .then(response => {
-            /*
-            checking if the promise resolved, if not creating an error message to increase 
-            error transparency to know how to debug (why it went wrong)
-            */
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status}`)
-            }
-            //when fetching from the given resource, the success default should be to return the response parsed with JSON
-            return response.json();
-            //taking the data or information within the response (information about ground types) and console logging all of ground type pokemon
-        }).then(data => {
-            console.log(data.pokemon)
-            //returning it so the promise results in having a value to offer to another promise chained to this promise, if anything 
-            return data.pokemon
-        }).then(pokemon => {
-            //returning a random pokemon of the ground type array by generating a number from 1 through 187 (representing the indexes of the object array the pokemon are held in)
-            const randomNum = Math.ceil(Math.random() * 188)
-            //returning the pokemon at the random given index generated by randomNum()
-            console.log(pokemon[randomNum - 1])
-        })
-        //catching the error and throwing it back???
-        .catch(error => {
-            console.error('Could not fetch pokemon:', error);
-            throw error; // Re-throw the error to ensure the promise is rejected?? I need more explanation 
-        });
+//     //returning a fetch promise
+//     return fetch(urlGround)
+//         //chaining .then promises 
+//         .then(response => {
+//             /*
+//             checking if the promise resolved, if not creating an error message to increase 
+//             error transparency to know how to debug (why it went wrong)
+//             */
+//             if (!response.ok) {
+//                 throw new Error(`Error: ${response.status}`)
+//             }
+//             //when fetching from the given resource, the success default should be to return the response parsed with JSON
+//             return response.json();
+//             //taking the data or information within the response (information about ground types) and console logging all of ground type pokemon
+//         }).then(data => {
+//             console.log(data.pokemon)
+//             //returning it so the promise results in having a value to offer to another promise chained to this promise, if anything 
+//             return data.pokemon
+//         }).then(pokemon => {
+//             //returning a random pokemon of the ground type array by generating a number from 1 through 187 (representing the indexes of the object array the pokemon are held in)
+//             const randomNum = Math.ceil(Math.random() * 188)
+//             //returning the pokemon at the random given index generated by randomNum()
+//             console.log(pokemon[randomNum - 1])
+//         })
+//         //catching the error and throwing it back???
+//         .catch(error => {
+//             console.error('Could not fetch pokemon:', error);
+//             throw error; // Re-throw the error to ensure the promise is rejected?? I need more explanation 
+//         });
 
 
-}
+// }
 
-typeTest();
+// typeTest();
 
 
 
